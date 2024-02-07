443c048083b8f483a42ed3b7a77bf0ca928af312
Small <154618905+ousmall@users.noreply.github.com>
2024-02-07 19:42:41 -0400

    Update main.py

--- Python-for-day45-top-100-moives/main.py
+++ Python-for-day45-top-100-moives/main.py
@@ -24,24 +24,3 @@ for each_movie in movie_list:
     except FileNotFoundError:
         with open("movies.txt", "w") as file:
             file.write(top_100_list)
-
-
-# movie_list = []
-#
-# for each_name in movie_names:
-#     movie_name = each_name.get_text()
-#     movie_list.append(movie_name)
-#
-# for index, name in enumerate(reversed(movie_list), 1):
-#     # "enumerate" to get index, "reversed" to upside down
-#     if name.endswith(", The"):
-#         name = "The " + name.split(", ")[0]
-#     movie_name = name
-#     top_100_list = f"{index}) {movie_name}\n"
-#
-#     try:
-#         with open("movies.txt", "a") as file:
-#             file.write(top_100_list)
-#     except FileNotFoundError:
-#         with open("movies.txt", "w") as file:
-#             file.write(top_100_list)
