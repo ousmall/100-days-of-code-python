ae787d615d4c8b67c18d74495f2962d236cc0467
Small <154618905+ousmall@users.noreply.github.com>
2024-03-06 12:32:12 -0400

    Update main.py

--- Python-for-day60-blog_html_form/main.py
+++ Python-for-day60-blog_html_form/main.py
@@ -8,7 +8,7 @@ SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 SECURITY = "TLS"  # SSL/TLS
 MY_EMAIL = "smallouforme@gmail.com"
-MY_PASS = "vwvepjpzahhreglr"
+MY_PASS = "secret"
 
 # blog contents
 ARTICLE = "https://api.npoint.io/886b0c394633b04e2704"
